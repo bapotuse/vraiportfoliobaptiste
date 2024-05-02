19b16c688279d5ebcc23823832d6e22fb62d6b8e
baptoste <77754709+baptoste@users.noreply.github.com>
2024-05-02 21:13:16 +0200

    Add files via upload

---
+++ customScript.js
@@ -0,0 +1,5 @@
+bindedFields.push({name: "lien"});
+bindedFields.push({name :"rep"});
+bindedFields.push({name :"lientheme1"});
+bindedFields.push({name :"lientheme2"});
+bindedFields.push({name :"lientheme3"});
